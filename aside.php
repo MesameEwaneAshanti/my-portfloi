73cc9820fe27c4f715424bee35ade99e1541841b
MesameEwaneAshanti <mesameewane.ashanti@ictuniversity.edu.cm>
2025-08-28 11:06:37 +0100

    Add files via upload

---
+++ aside.php
@@ -0,0 +1,11 @@
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+        <aside>
+            <h1>Logo</h1>
+            <div class="links">
+                <div>
+                     <span class="material-icons">home</span>
+                     <a href="./contact.html">Dashboard</a>
+                </div>
+            </div>
+        </aside>
